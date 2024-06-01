495efcb96bab41706fcdca3baa359ffd416ae8ab
Egor Vetkin <vetkin.yegor@yandex.ru>
2024-06-01 18:40:42 +0300

    После замечаний ревьюера. ver 1.2

--- scripts/index.js
+++ scripts/index.js
@@ -5,11 +5,13 @@ const cardTemplate = document.querySelector('#card-template').content;
 const cardsConteiner = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-function createCard(evt, deleteCard) {
+function createCard(cardData, deleteCard) {
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-  cardElement.querySelector('.card__image').src = evt.link;
-  cardElement.querySelector('.card__title').textContent = evt.name;
-  cardElement.querySelector('.card__image').alt = evt.name;
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  cardImage.src = cardData.link;
+  cardTitle.textContent = cardData.name;
+  cardImage.alt = cardData.name;
 
   const deleteButton = cardElement.querySelector('.card__delete-button');
   deleteButton.addEventListener('click', deleteCard);
@@ -23,8 +25,8 @@ function deleteCard(evt) {
 }
 
 // @todo: Вывести карточки на страницу
-initialCards.forEach(function (evt) {
-  const card = createCard(evt, deleteCard);
+initialCards.forEach(function (cardData) {
+  const card = createCard(cardData, deleteCard);
   cardsConteiner.append(card);
 })
 
