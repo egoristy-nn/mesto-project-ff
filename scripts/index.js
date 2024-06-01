84ee13444a3b2ea97b494ce6bf7242815763be06
Egor Vetkin <vetkin.yegor@yandex.ru>
2024-06-01 13:44:12 +0300

    Проектная работа выполнена. ver 1.0

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const cardsConteiner = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(evt, deleteCard) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  cardElement.querySelector('.card__image').src = evt.link;
+  cardElement.querySelector('.card__title').textContent = evt.name;
+  cardElement.querySelector('.card__image').alt = evt.name;
+
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', deleteCard);
+
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(evt) {
+    evt.target.closest('.places__item').remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function (evt) {
+  const card = createCard(evt, deleteCard);
+  cardsConteiner.append(card);
+})
+
